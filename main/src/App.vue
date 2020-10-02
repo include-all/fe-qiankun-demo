323983eef8664f1a4ecf3ee61d80c500e947d253
include-all <1379002028@qq.com>
2020-10-02 20:37:16 +0800

    test webhooks

--- main/src/App.vue
+++ main/src/App.vue
@@ -22,7 +22,7 @@
       </el-menu>
     </div>
     <div class="layout-content">
-      <div class="header">test webkooks1</div>
+      <div class="header">test webkooks</div>
       <div id="sub-app-root"></div>
     </div>
   </div>
